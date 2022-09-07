f7c0dbad1c015fe30ac68cf528c8384cfa2e2d2c
Oksana Hludneva <oksana.hludneva@gmail.com>
2022-09-07 19:43:39 +0300

    Twilio first commit

--- src/main/java/com/example/eas_finalproject/services/MessagingService.java
+++ src/main/java/com/example/eas_finalproject/services/MessagingService.java
@@ -1,4 +1,36 @@
 package com.example.eas_finalproject.services;
 
+import com.twilio.Twilio;
+import com.twilio.exception.ApiException;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import com.twilio.exception.AuthenticationException;
+import com.twilio.rest.notify.v1.service.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Arrays;
+import java.util.List;
+
 public class MessagingService {
+
+    static Logger logger = LoggerFactory.getLogger(MessagingService.class);
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+    public static final String SERVICE_SID = System.getenv("NOTIFY_SERVICE_SID");
+    public static void main(String[] args) throws AuthenticationException {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        List<String> toBindings = Arrays.asList(
+                "{\"binding_type\":\"sms\",\"address\":\"+371...\"}",
+                "{\"binding_type\":\"sms\",\"address\":\"+371...\"}");
+
+        Notification notification = Notification
+                .creator(SERVICE_SID)
+                .setBody("SMS text")
+                .setToBinding(toBindings)
+                .create();
+
+        System.out.println(notification.getSid());
+    }
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,16 @@ module com.example.eas_finalproject {
     requires lombok;
     requires java.sql;
     requires commons.configuration;
-
+    requires twilio;
+    requires org.slf4j;
+    requires dotenv.java;
 
     opens com.example.eas_finalproject to javafx.fxml;
     exports com.example.eas_finalproject;
 
     exports com.example.eas_finalproject.controllers;
     opens com.example.eas_finalproject.controllers;
+
+    exports com.example.eas_finalproject.services;
+    opens com.example.eas_finalproject.services;
 }
