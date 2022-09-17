b2591031020f06df4542439501cff8b3882d34c2
Laura Rutkovska <laurarutkovska@inbox.lv>
2022-09-17 11:01:36 +0300

    Enter button add update commit

--- src/main/java/com/example/eas_finalproject/controllers/LoginController.java
+++ src/main/java/com/example/eas_finalproject/controllers/LoginController.java
@@ -4,10 +4,12 @@ import com.example.eas_finalproject.repository.DataRepository;
 import com.example.eas_finalproject.services.SceneService;
 import com.example.eas_finalproject.services.UserService;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 
 public class LoginController {
     @FXML
@@ -20,18 +22,7 @@ public class LoginController {
     public void onUserLoginClick(ActionEvent actionEvent) {
 
         try {
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-
-            int userId = this.userService.verifyUserDetails(username, password);
-
-            DataRepository.getInstance().setLoggedInUserId(userId);
-            SceneService.showAlert(
-                    "Login success",
-                    "Logged in successfully",
-                    Alert.AlertType.CONFIRMATION
-            );
-            DataRepository.getInstance().setLoggedInUserId(userId);
+            logInDetails();
             SceneService.changeScene(actionEvent, "messaging");
         }catch (Exception exception){
             SceneService.showAlert("Login Failed", exception.getMessage(), Alert.AlertType.ERROR);
@@ -41,5 +32,37 @@ public class LoginController {
     @FXML
     protected void onUserRegisterClick(ActionEvent actionEvent){
         SceneService.changeScene(actionEvent, "registerSecurityCode");
+    }
+
+    @FXML
+    public void onEnterPressed(ActionEvent actionEvent) {
+
+        try {
+            logInDetails();
+
+            passwordField.setOnKeyPressed(keyEvent -> {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    SceneService.changeScene(actionEvent, "messaging");
+                }
+            });
+
+        } catch (Exception exception) {
+            SceneService.showAlert("Login Failed", exception.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void logInDetails() throws Exception {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        int userId = this.userService.verifyUserDetails(username, password);
+
+        DataRepository.getInstance().setLoggedInUserId(userId);
+        SceneService.showAlert(
+                "Login success",
+                "Logged in successfully",
+                Alert.AlertType.CONFIRMATION
+        );
+        DataRepository.getInstance().setLoggedInUserId(userId);
     }
 }
